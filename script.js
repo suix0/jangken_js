fa7c8ebafcf1abda24a24bcf7715740ebd22ed05
suix0 <112095627+suix0@users.noreply.github.com>
2023-12-06 11:56:27 +0800

    finsihed jangken script wahhaa

--- script.js
+++ script.js
@@ -20,12 +20,47 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toUpperCase();
   computerSelection = computerSelection.toUpperCase();
 
-  // define rules to determine winner
-  // starting with rock
-  let result = `User: ${playerSelection}\nComputer: ${computerSelection}\nResult: `;
+  // define cases to determine winner
+  let result = `You: ${playerSelection}\nComputer: ${computerSelection}\nResult: `;
 
-  // draw if both are draw
+  // starting with cases when user picks ROCK
   if (playerSelection === "ROCK" && computerSelection === "ROCK") {
     return result.concat('Tie');
+  } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+    return result.concat("Computer Win");
+  } else if (playerSelection === "ROCK" && computerSelection === "SCISSOR") {
+    return result.concat("You WIN");
+
+  // now check cases when user chooses PAPER
+  } else if (playerSelection === "PAPER" && computerSelection === "PAPER") {
+    return result.concat("Tie");
+  } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+    return result.concat("You WIN");
+  } else if (playerSelection === "PAPER" && computerSelection === "SCISSOR") {
+    return result.concat("Computer WIN");
+
+  // lastly, check cases for when user chooses SCISSOR
+  } else if (playerSelection === "SCISSOR" && computerSelection === "SCISSOR") {
+    return result.concat("Tie");
+  } else if (playerSelection === "SCISSOR" && computerSelection === "PAPER") {
+    return result.concat("You WIN");
+  } else if (playerSelection === "SCISSOR" && computerSelection === "ROCK") {
+    return result.concat("Computer WIN");
+  } else {
+    alert("Please enter a valid input.");
+    return -1;
+  }
+
+}
+
+function game() {
+  let playerChoice;
+  let computerChoice;
+  for (let i = 0; i < 5; i++) {
+    playerChoice = prompt("Rock, Paper, Scissor?");
+    computerChoice = getComputerChoice();
+    console.log(playRound(playerChoice, computerChoice));
   }
 }
+
+game();
